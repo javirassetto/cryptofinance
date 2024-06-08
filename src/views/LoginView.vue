cd6b2444ee7d78a25643472d1deb570b12aaa14d
Javier Rassetto <javi.rassetto@gmail.com>
2024-06-08 00:55:05 -0300

    create Login View

---
+++ src/views/LoginView.vue
@@ -0,0 +1,26 @@
+<template>
+    
+    <div class="login-view">        
+        <Login /> 
+    </div>
+</template>
+<script>
+import Login from '../components/Login.vue';
+
+export default {
+    name:'LoginView',
+    components: {
+     Login,
+    },
+
+};
+</script>
+<style scoped>
+.login-view{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+
+</style>
