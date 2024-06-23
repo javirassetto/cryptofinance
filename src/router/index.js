5241b411fa1e5a0b1349684fcab406d86cead288
Javier Rassetto <javi.rassetto@gmail.com>
2024-06-23 17:37:14 -0300

    modified Views and configure routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import BuyView from "@/views/BuyView";
 import LoginView from "@/views/LoginView.vue";
-import AboutView from "@/views/AboutView";
+import SellView from "@/views/SellView";
 
 const routes = [
   {
@@ -16,12 +16,12 @@ const routes = [
   {
     path: "/buyCripto",
     name: "buyCripto",
-    component: HomeView,
+    component: BuyView,
   },
   {
     path: "/sellCripto",
     name: "sellCripto",
-    component: AboutView,
+    component: SellView,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
