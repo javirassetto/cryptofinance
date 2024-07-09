af20e7ad22104aede240b2b09af9237095609ab3
Javier Rassetto <javi.rassetto@gmail.com>
2024-07-08 23:51:41 -0300

    create utils/calculatePrice.js

---
+++ src/utils/calculatePrice.js
@@ -0,0 +1,21 @@
+import { getCryptoPrice } from "@/services/apiService";
+
+export async function calculatePrice(crypto_code, crypto_amount, action) {
+  if (crypto_code && crypto_amount && action) {
+    try {
+      const cryptoValue = await getCryptoPrice(crypto_code, action);
+      const calculatedMoney = crypto_amount * cryptoValue;
+      // error de consola el valor calculado es un número válido
+      if (!isNaN(calculatedMoney)) {
+        return calculatedMoney.toFixed(2);
+      } else {
+        return "";
+      }
+    } catch (error) {
+      console.error("Error al calcular el precio:", error);
+      return "";
+    }
+  } else {
+    return "";
+  }
+}
