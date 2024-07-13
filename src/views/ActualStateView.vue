b7e1155347851dae6b0f3806129c0c013ef21070
Javier Rassetto <javi.rassetto@gmail.com>
2024-07-13 18:24:30 -0300

    create ActualState view

---
+++ src/views/ActualStateView.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2>Estado actual de tus finanzas</h2>
+  <div>
+    <ActualState />
+  </div>
+</template>
+
+<script>
+import ActualState from "@/components/ActualState.vue";
+
+export default {
+  name: "ActualStateView",
+  components: {
+    ActualState,
+  },
+};
+</script>
