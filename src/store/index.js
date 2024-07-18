e735c48d159ba3350f96d80417c53b38565374f8
Javier Rassetto <javi.rassetto@gmail.com>
2024-07-18 20:53:30 -0300

    git commit -m git commit -m

--- src/store/index.js
+++ src/store/index.js
@@ -8,12 +8,14 @@ export default createStore({
     user: StoredUser ? JSON.parse(StoredUser) : null,
   },
   mutations: {
+    /*actualizo el state user y también guardo el usuario en localStorage*/
     setUser(state, user) {
       state.user = user;
       localStorage.setItem("user", JSON.stringify(user));
     },
   },
   actions: {
+    /*accion que reacciona al Login "dispatch" */
     login({ commit }, user) {
       commit("setUser", user);
     },
@@ -23,6 +25,7 @@ export default createStore({
       localStorage.removeItem("user");
     },
   },
+  //para poder acceder a state
   getters: {
     getUser(state) {
       return state.user;
